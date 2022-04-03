dabfc812fcdd4d8541fca9919098c47f702a34c9
AlanZhangAoYu <z7954221907482@163.com>
2022-04-03 21:16:53 +0800

    加入音乐工具类

---
+++ music-service/src/main/java/util/Mp3Util.java
@@ -0,0 +1,32 @@
+package util;
+
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.mp3.MP3AudioHeader;
+import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
+import java.io.File;
+import java.util.HashMap;
+
+/**
+ * @author ZAY
+ */
+public class Mp3Util {
+    public static HashMap<String,String> getMp3Info(String path) throws Exception{
+        MP3File mp3File = (MP3File) AudioFileIO.read(new File(path));
+        AbstractID3v2Tag abstractId3v2Tag = mp3File.getID3v2Tag();
+        MP3AudioHeader mp3AudioHeader = mp3File.getMP3AudioHeader();
+
+        String songName = abstractId3v2Tag.getFirst(FieldKey.TITLE);
+        String artist = abstractId3v2Tag.getFirst(FieldKey.ARTIST);
+        String album = abstractId3v2Tag.getFirst(FieldKey.ALBUM);
+        int length = mp3AudioHeader.getTrackLength();
+
+        HashMap<String,String> map=new HashMap<>(4);
+        map.put("songName",songName);
+        map.put("artist",artist);
+        map.put("album",album);
+        map.put("length",length / 60+":"+(length % 60 < 10 ? "0"+length % 60 : length % 60));
+        return map;
+    }
+}
--- music-service/src/test/java/MyTest.java
+++ music-service/src/test/java/MyTest.java
@@ -1,7 +1,8 @@
 import org.junit.Test;
 import util.Md5Util;
+import util.Mp3Util;
 import util.RandomSaltUtil;
-
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class MyTest {
@@ -13,5 +14,14 @@ public class MyTest {
     @Test
     public void RandomSaltTest(){
         System.out.println(RandomSaltUtil.generateRandomCode());
+    }
+    @Test
+    public void Mp3Test(){
+        try {
+            HashMap<String,String> map= Mp3Util.getMp3Info("F:\\音乐\\G.E.M.邓紫棋 - 龙卷风.mp3");
+            System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
