834d39d595756b56c0b685e53ec5b312fc4e5006
AlanZhangAoYu <z7954221907482@163.com>
2022-04-25 19:39:39 +0800

    Revert "上传歌手图像"
    This reverts commit d7ec6c4b51075fe89b14feceba2eaf2948c79b01.

--- music-service/src/main/java/controller/SingerController.java
+++ music-service/src/main/java/controller/SingerController.java
@@ -2,7 +2,10 @@ package controller;
 
 import com.alibaba.fastjson.JSON;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import pojo.Singer;
 import service.MongoDbFileService;
@@ -34,28 +37,25 @@ public class SingerController {
         map.put("AllSingerCount", singerService.selectAllSingerCount());
         return JSON.toJSONString(map);
     }
-    @PostMapping(value = "/updateSingerImg")
+    @PostMapping(value = "/updateSingerInfo")
     @Transactional(rollbackFor = Exception.class)
-    public String updateSingerImgController(@RequestParam("file") MultipartFile file,@RequestParam("singerId") int singerId){
-        HashMap<Object,Object> map=new HashMap<>(1);
-        map.put("singerId",singerId);
+    public String updateSingerInfoController(@RequestParam("file") MultipartFile file,
+                                             @RequestParam("singerId") int singerId,
+                                             @RequestParam("singerName") String singerName,
+                                             @RequestParam("singerBirth") Timestamp singerBirth,
+                                             @RequestParam("singerIntroduction") String singerIntroduction,
+                                             @RequestParam("singerLocation") String singerLocation){
+        HashMap<Object,Object> map=new HashMap<>(6);
         HashMap<String,String> resultMap=new HashMap<>(1);
         if(!file.isEmpty()){
             String fileId= mongoDbFileService.uploadFile(file);
             map.put("singerImgId",fileId);
         }
-        int result = singerService.updateSingerInfo(map);
-        if(result == 1){
-            resultMap.put("msg", GlobalConstant.SUCCESS);
-        }else {
-            resultMap.put("msg",GlobalConstant.FAIL_STR);
-        }
-        return JSON.toJSONString(resultMap);
-    }
-    @PostMapping(value = "/updateSingerInfo")
-    @Transactional(rollbackFor = Exception.class)
-    public String updateSingerInfoController(@RequestBody HashMap<Object,Object> map){
-        HashMap<String,String> resultMap=new HashMap<>(1);
+        map.put("singerId",singerId);
+        map.put("singerName",singerName);
+        map.put("singerBirth",singerBirth);
+        map.put("singerIntroduction",singerIntroduction);
+        map.put("singerLocation",singerLocation);
         int result = singerService.updateSingerInfo(map);
         if(result == 1){
             resultMap.put("msg", GlobalConstant.SUCCESS);
