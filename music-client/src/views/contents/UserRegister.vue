6e6fe23e2feee2d82f4fcb4fe1ebd2a0ecfde709
AlanZhangAoYu <z7954221907482@163.com>
2022-06-02 16:20:09 +0800

    用户注册v0.1

--- music-client/src/views/contents/UserRegister.vue
+++ music-client/src/views/contents/UserRegister.vue
@@ -1,5 +1,13 @@
 <template>
-<div>用户注册</div>
+  <div class="main">
+    <div class="font_title">
+      <div style="font-style: oblique;font-size: 30px;color: #f9f9f9">Zay-music</div>
+      <div style="font-size: 80px;color: #f9f9f9">-&nbsp;用心聆听生活</div>
+      <div style="font-size: 80px;color: #f9f9f9">-&nbsp;用爱感受美好</div>
+    </div>
+    <div class="register_window">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +17,20 @@ export default {
 </script>
 
 <style scoped>
-
+.main{
+  background-image: url("../../assets/image/reg_bg_min.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  width: 100%;
+  height: 100vh;
+}
+.register_window{
+  background-color: rgba(255,255,255,0.5);
+  height: 600px;
+  width: 500px;
+  border-radius: 10px;
+  position: absolute;
+  top: 100px;
+  left: 60%;
+}
 </style>
